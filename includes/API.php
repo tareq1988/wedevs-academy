f11c2e5d06f690db9dab39d2aadb93bc4064fb54
Tareq Hasan <tareq1988@gmail.com>
2020-04-18 11:38:14 +0800

    Added docblock in API class

--- includes/API.php
+++ includes/API.php
@@ -7,10 +7,18 @@ namespace WeDevs\Academy;
  */
 class API {
 
+    /**
+     * Initialize the class
+     */
     function __construct() {
         add_action( 'rest_api_init', [ $this, 'register_api' ] );
     }
 
+    /**
+     * Register the API
+     *
+     * @return void
+     */
     public function register_api() {
         $addressbook = new API\Addressbook();
         $addressbook->register_routes();
